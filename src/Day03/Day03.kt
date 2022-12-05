db01f9276d3fa6506918df08781e91239097eae7
hdelgado <hector.delgado@lastminute.com>
2022-12-05 22:55:06 +0100

    [aoc-2022] Day3 part one

--- src/Day03/Day03.kt
+++ src/Day03/Day03.kt
@@ -4,19 +4,42 @@ import java.io.File
 
 fun main() {
 
+    val chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    fun Char.toValue(): Int = chars.indexOf(this) + 1
+    check('p'.toValue() == 16)
+    check('L'.toValue() == 38)
+
     fun part1(input: String): Int {
-        TODO()
+
+        var commonChars = mutableListOf<Char>()
+        input.lines()
+            .map { it.substring(0, it.length.div(2)) to it.substring(it.length.div(2), it.length) }
+            .map {
+                it.first.iterator().forEach { char ->
+                    if (it.second.contains(char)) {
+                        commonChars.add(char)
+                        return@map
+                    }
+                }
+            }
+
+        return commonChars.map { it.toValue() }.sum()
     }
+
 
     fun part2(input: String): Int {
         TODO()
     }
 
-    // test if implementation meets criteria from the description, like:
+
     val testInput = File("src/Day03/test_data.txt").readText()
+    println(part1(testInput))
     check(part1(testInput) == 157)
 
     val input = File("src/Day03/data.txt").readText()
     println(part1(input))
+
 }
+
 
